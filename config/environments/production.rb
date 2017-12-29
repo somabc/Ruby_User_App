f8062a089f6511c24d1525041e60db684cbf4d7f
bryan <somabc@gmail.com>
2017-12-29 13:10:44 +0000

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,10 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
