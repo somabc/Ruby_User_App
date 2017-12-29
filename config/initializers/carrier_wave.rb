4379e9ab1aebce111ddeca2106d856ca08650a4a
bryan <somabc@gmail.com>
2017-12-29 22:08:01 +0000

    fog updated

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,8 @@
 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
